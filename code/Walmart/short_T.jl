40903e2b7048c43580e9e10e7ad80461ce833f94
Kyle Butts <buttskyle96@gmail.com>
2024-02-25 12:48:29 -0500

    Add an additional esitmation using truncated sample:
    - Trims sample to 1977-1992.
    - Uses just 1986-1992 treated units  and redefine counties treated after 1992 as never-treated
    - Reestimate using smaller sample

---
+++ code/Walmart/short_T.jl
@@ -0,0 +1,213 @@
+#' ---
+#' title: 'Short T0'
+#' --- 
+#' This script uses just 1986-1992 treated units (trimming sample a bunch). 
+#' This might improve efficiency with more “never treated” while highlighting 
+#' the advantage of the method (short panels)
+
+# %% 
+#| label: setup
+using BlockDiagonals
+using Calculus
+using CategoricalArrays
+using CSV
+using DataFrames
+using Dates
+using Distributions
+using FixedEffectModels
+using ForwardDiff
+using GLM
+using LinearAlgebra
+using LineSearches
+using Optim
+using Plots
+using ProjectRoot
+using Random
+using SparseArrays
+using Statistics
+using StatsBase
+
+here = @projectroot() 
+cd(here)
+include("$here/code/Walmart/qld_helpers.jl")
+
+# Set parameters
+YEARS = collect(1977:1999)
+T0 = 1985
+file = "data/County_Business_Patterns/sample_basker_YEARS_$(minimum(YEARS))_$(maximum(YEARS))_T0_$(T0).csv"
+
+main = DataFrame(CSV.File(file))
+
+# Sample to years 1977:1992
+df = main[main.year .<= 1992, :]
+df[df.g .> 1992, :rel_year] .= -Inf
+df[df.g .> 1992, :g] .= Inf
+
+# %% 
+YEARS = collect(1977:1992)
+T = length(YEARS)
+rel_years = begin
+  x = df[df.rel_year .!= -Inf, :rel_year]
+  collect(minimum(x):maximum(x))
+end
+
+# %%
+# Order by fips, then by year
+sort!(df, [:fips, :year])
+
+# %% Estimate
+tau_hat_retail_p_0 = est_te(
+  df, 0, :log_retail_emp; export_y0=true, outcome="retail", quick_plot=true
+)
+tau_hat_retail_p_1 = est_te(
+  df, 1, :log_retail_emp; export_y0=true, outcome="retail", quick_plot=true
+)
+tau_hat_retail_p_2 = est_te(
+  df, 2, :log_retail_emp; export_y0=true, outcome="retail", quick_plot=true
+)
+tau_hat_retail_p_3 = est_te(
+  df, 3, :log_retail_emp; export_y0=true, outcome="retail", quick_plot=true
+)
+tau_hat_retail_p_4 = est_te(
+  df, 4, :log_retail_emp; export_y0=true, outcome="retail", quick_plot=true
+)
+
+# %% 
+tau_hat_wholesale_p_0 = est_te(
+  df, 0, :log_wholesale_emp; export_y0=true, outcome="wholesale"
+)
+tau_hat_wholesale_p_1 = est_te(
+  df, 1, :log_wholesale_emp; export_y0=true, outcome="wholesale"
+)
+tau_hat_wholesale_p_2 = est_te(
+  df, 2, :log_wholesale_emp; export_y0=true, outcome="wholesale"
+)
+tau_hat_wholesale_p_3 = est_te(
+  df, 3, :log_wholesale_emp; export_y0=true, outcome="wholesale"
+)
+tau_hat_wholesale_p_4 = est_te(
+  df, 4, :log_wholesale_emp; export_y0=true, outcome="wholesale"
+)
+
+# %% 
+# Ahn, Lee, and Schmidt procedure for selecting p
+println("""
+Checking for p for retail employment: 
+p = 0; Hansen-Sargant Test p-value = $(tau_hat_retail_p_0.sargan[2])
+p = 1; Hansen-Sargant Test p-value = $(tau_hat_retail_p_1.sargan[2])
+p = 2; Hansen-Sargant Test p-value = $(tau_hat_retail_p_2.sargan[2])
+p = 3; Hansen-Sargant Test p-value = $(tau_hat_retail_p_3.sargan[2])
+p = 4; Hansen-Sargant Test p-value = $(tau_hat_retail_p_4.sargan[2])
+
+Checking for p for wholesale employment: 
+p = 0; Hansen-Sargant Test p-value = $(tau_hat_wholesale_p_0.sargan[2])
+p = 1; Hansen-Sargant Test p-value = $(tau_hat_wholesale_p_1.sargan[2])
+p = 2; Hansen-Sargant Test p-value = $(tau_hat_wholesale_p_2.sargan[2])
+p = 3; Hansen-Sargant Test p-value = $(tau_hat_wholesale_p_3.sargan[2])
+p = 4; Hansen-Sargant Test p-value = $(tau_hat_wholesale_p_4.sargan[2])
+""")
+
+# %% Chose p based on p-values
+retail_p_values = (
+  tau_hat_retail_p_0.sargan[2],
+  tau_hat_retail_p_1.sargan[2],
+  tau_hat_retail_p_2.sargan[2],
+  tau_hat_retail_p_3.sargan[2],
+  tau_hat_retail_p_4.sargan[2],
+)
+retail_p = findfirst(retail_p_values .> 0.1) - 1
+retail_est = eval(Symbol("tau_hat_retail_p_$(retail_p)"))
+
+wholesale_p_values = (
+  tau_hat_wholesale_p_0.sargan[2],
+  tau_hat_wholesale_p_1.sargan[2],
+  tau_hat_wholesale_p_2.sargan[2],
+  tau_hat_wholesale_p_3.sargan[2],
+  tau_hat_wholesale_p_4.sargan[2],
+)
+wholesale_p = findfirst(wholesale_p_values .> 0.1) - 1
+wholesale_est = eval(Symbol("tau_hat_wholesale_p_$(wholesale_p)"))
+
+
+# %% Retail bootstrap (takes about 10 minutes)
+# display current time
+start_time = Dates.now()
+println("Current Time: $(Dates.format(start_time, "HH:MM:SS"))")
+Random.seed!(123)
+
+B = 1000
+ids = unique(df[:, :fips])
+retail_ests = zeros(B, length(rel_years))
+retail_ests[1, :] = retail_est.est[1]'
+
+b = 2
+while b < B
+  b % 50 == 0 && println("On Bootstrap Iteration: $(b)")
+
+  resample_ids = StatsBase.sample(ids, length(ids))
+  resample_df = vcat([df[df.fips .== id, :] for id in resample_ids]...)
+  resample_df.fips = repeat(1:length(ids); inner=T)
+
+  try
+    boot_est = est_te(
+      resample_df,
+      retail_p,
+      :log_retail_emp;
+      export_y0=false,
+      outcome="retail",
+      quick_plot=false,
+    )
+    retail_ests[b, :] = boot_est.est[1]'
+    b += 1
+  catch
+    println("Error on bootstrap iteration: $(b)")
+  end
+end
+
+CSV.write(
+  "estimates/short_t_qld_p_$(retail_p)_est_retail_B_$(B).csv",
+  DataFrame(retail_ests, ["tau$(i)" for i in rel_years]),
+)
+end_time = Dates.now()
+print("Current Time: $(Dates.format(end_time, "HH:MM:SS"))")
+
+# %% Wholesale bootstrap (takes about 10 minutes)
+
+# display current time
+start_time = Dates.now()
+println("Current Time: $(Dates.format(start_time, "HH:MM:SS"))")
+B = 1000
+ids = unique(df[:, :fips])
+wholesale_ests = zeros(B, length(rel_years))
+wholesale_ests[1, :] = wholesale_est.est[1]'
+
+b = 2
+while b < B
+  b % 50 == 0 && println("On Bootstrap Iteration: $(b)")
+
+  resample_ids = StatsBase.sample(ids, length(ids))
+  resample_df = vcat([df[df.fips .== id, :] for id in resample_ids]...)
+  resample_df.fips = repeat(1:length(ids); inner=T)
+
+  try
+    boot_est = est_te(
+      resample_df,
+      wholesale_p,
+      :log_wholesale_emp;
+      export_y0=false,
+      outcome="wholesale",
+      quick_plot=false,
+    )
+    wholesale_ests[b, :] = boot_est.est[1]'
+    b += 1
+  catch
+    println("Error on bootstrap iteration: $(b)")
+  end
+end
+
+CSV.write(
+  "estimates/short_t_qld_p_$(wholesale_p)_est_wholesale_B_$(B).csv",
+  DataFrame(wholesale_ests, ["tau$(i)" for i in rel_years]),
+)
+end_time = Dates.now()
+print("Current Time: $(Dates.format(end_time, "HH:MM:SS"))")
---
+++ code/Walmart/short_T_figures.R
@@ -0,0 +1,153 @@
+#' ---
+#' title: "Create figures for Walmart application"
+#' author: "Kyle Butts and Nicholas Brown"
+#' ---
+
+# %% setup
+#| warning: false
+library(tidyverse)
+library(data.table)
+library(ggplot2)
+library(here)
+library(fixest)
+library(kfbmisc)
+
+source(here("figures/convert_tex_to_pdf.R"))
+
+
+# Load Estimates ---------------------------------------------------------------
+# %%
+short_t_qld_retail <- fread(here("estimates/short_t_qld_p_3_est_retail_B_5000.csv"))
+short_t_qld_retail <- as.matrix(short_t_qld_retail)
+short_t_qld_retail <- data.table(
+  rel_year = -15L:6L,
+  estimate = short_t_qld_retail[1, ],
+  std_error = drop(apply(short_t_qld_retail, 2, sd)),
+  lower = drop(apply(short_t_qld_retail, 2, quantile, probs = 0.025)),
+  upper = drop(apply(short_t_qld_retail, 2, quantile, probs = 0.975))
+)
+short_t_qld_retail$pre <- short_t_qld_retail$rel_year < 0
+short_t_qld_retail$lower_normal <-
+  short_t_qld_retail$estimate - 1.96 * short_t_qld_retail$std_error
+short_t_qld_retail$upper_normal <-
+  short_t_qld_retail$estimate + 1.96 * short_t_qld_retail$std_error
+
+# %%
+# Estimate pre-trend
+pre_short_t_qld_retail <- coef(feols(
+  estimate ~ rel_year, short_t_qld_retail[rel_year < 0 & rel_year >= -15]
+))
+
+(plot_short_t_qld_retail <- ggplot() +
+  geom_abline(
+    slope = 0, intercept = 0,
+    linetype = "dashed", linewidth = 0.8
+  ) +
+  geom_abline(
+    slope = pre_short_t_qld_retail[2], intercept = pre_short_t_qld_retail[1],
+    color = "#9A2415",
+    linewidth = 2
+  ) +
+  geom_point(
+    aes(x = rel_year, y = estimate, color = pre),
+    data = short_t_qld_retail,
+    size = 3
+  ) +
+  # Empirical 95%
+  # geom_errorbar(
+  #   aes(x = rel_year, ymin = lower, ymax = upper, color = pre),
+  #   data = short_t_qld_retail,
+  #   width = 0.6, linewidth = 2
+  # ) +
+  # 1.96 * standard error
+  geom_errorbar(
+    aes(x = rel_year, ymin = lower_normal, ymax = upper_normal, color = pre),
+    data = short_t_qld_retail,
+    width = 0.6, linewidth = 2
+  ) +
+  labs(
+    x = "Event Time", y = NULL
+  ) +
+  scale_y_continuous(limits = c(-0.2, 0.3), expand = c(0,0)) +
+  scale_x_continuous(limits = c(-22, 13)) +
+  scale_color_manual(
+    values = c("#107895", "#9A2415"),
+    guide = "none"
+  ) +
+  kfbmisc::theme_kyle(base_size = 16)
+)
+
+# %% 
+kfbmisc::tikzsave(
+  here("figures/short_t_qld_retail.pdf"),
+  plot_short_t_qld_retail, width = 10, height = 5
+)
+
+# %% 
+short_t_qld_wholesale <- fread(here("estimates/short_t_qld_p_2_est_wholesale_B_5000.csv"))
+short_t_qld_wholesale <- as.matrix(short_t_qld_wholesale)
+short_t_qld_wholesale <- short_t_qld_wholesale[apply(short_t_qld_wholesale, 1, \(x) !any(is.nan(x))), ]
+short_t_qld_wholesale <- data.table(
+  rel_year = -15L:6L,
+  estimate = short_t_qld_wholesale[1, ],
+  std_error = drop(apply(short_t_qld_wholesale, 2, sd)),
+  lower = drop(apply(short_t_qld_wholesale, 2, quantile, probs = 0.025)),
+  upper = drop(apply(short_t_qld_wholesale, 2, quantile, probs = 0.975))
+)
+short_t_qld_wholesale$pre <- short_t_qld_wholesale$rel_year < 0
+short_t_qld_wholesale$lower_normal <-
+  short_t_qld_wholesale$estimate - 1.96 * short_t_qld_wholesale$std_error
+short_t_qld_wholesale$upper_normal <-
+  short_t_qld_wholesale$estimate + 1.96 * short_t_qld_wholesale$std_error
+
+# %%
+# Estimate pre-trend
+pre_short_t_qld_wholesale <- coef(feols(
+  estimate ~ rel_year, short_t_qld_wholesale[rel_year < 0 & rel_year >= -15]
+))
+
+
+(plot_short_t_qld_wholesale <- ggplot() +
+  geom_abline(
+    slope = 0, intercept = 0,
+    linetype = "dashed", linewidth = 0.8
+  ) +
+  geom_abline(
+    slope = pre_short_t_qld_wholesale[2], intercept = pre_short_t_qld_wholesale[1],
+    color = "#9A2415",
+    linewidth = 2
+  ) +
+  geom_point(
+    aes(x = rel_year, y = estimate, color = pre),
+    data = short_t_qld_wholesale,
+    size = 3
+  ) +
+  # Empirical 95%
+  # geom_errorbar(
+  #   aes(x = rel_year, ymin = lower, ymax = upper, color = pre),
+  #   data = short_t_qld_wholesale,
+  #   width = 0.6, linewidth = 2
+  # ) +
+  # 1.96 * standard error
+  geom_errorbar(
+    aes(x = rel_year, ymin = lower_normal, ymax = upper_normal, color = pre),
+    data = short_t_qld_wholesale,
+    width = 0.6, linewidth = 2
+  ) +
+  labs(
+    x = "Event Time", y = NULL
+  ) +
+  scale_y_continuous(limits = c(-0.5, 0.2), expand = c(0,0)) +
+  scale_x_continuous(limits = c(-22, 13)) +
+  scale_color_manual(
+    values = c("#107895", "#9A2415"),
+    guide = "none"
+  ) +
+  kfbmisc::theme_kyle(base_size = 16)
+)
+
+# %% 
+kfbmisc::tikzsave(
+  here("figures/short_t_qld_wholesale.pdf"),
+  plot_short_t_qld_wholesale, width = 10, height = 5
+)
